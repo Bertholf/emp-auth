d8e9338ba20504a25e44313475bb7d08468b1134
robertholf <rob@bertholf.com>
2018-01-29 16:34:24 -0800

    PUSH IT REAL GOOD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        $this->registerPolicies();
+
+        Passport::routes();
     }
 
     /**
